1b08e6a6d50b6c5dfbcc46abb1bc5e5ae2522d5d
James Lawler <github@jameslawler.com>
2015-09-05 16:41:08 +0200

    Add a second activity

--- app/src/main/java/com/jameslawler/design/DetailActivity.java
+++ app/src/main/java/com/jameslawler/design/DetailActivity.java
@@ -1,24 +1,22 @@
 package com.jameslawler.design;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -35,10 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        startActivity(intent);
     }
 }
